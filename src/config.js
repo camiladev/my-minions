459db4574eb2f28da9b3067fd0c1b0c7d83d7eed
Camila Matos <camila.mila148@gmail.com>
2021-05-24 09:30:32 -0300

    Configurando aws-amplify

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "img-produtos",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://8j9blp9bp9.execute-api.us-east-1.amazonaws.com/dev",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_GAuXeJKgt",
+      APP_CLIENT_ID: "1uub48hr7u3o8epl09icomcfn5",
+      IDENTITY_POOL_ID: "us-east-1:7d4dfe02-0477-4664-b36a-b71cd5363e33",
+    },
+  };
+  
+  export default config;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import { ProductsProvider } from './contexts/ProductsContext';
 import './index.css';
 import Home from './pages/Home';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "produtosDB",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
+
 
 
 ReactDOM.render(
